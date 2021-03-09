fc4319aac8ed1fde2bec6a62e0f78c33a97bf2f5
eshaaagrawal <2018mit010@sggs.ac.in>
2021-03-09 15:22:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,12 +10,10 @@ import Service from "./Service";
 import Login from "./Login";
 import Signup from "./Signup"; 
 import Footer from "./Footer";
-import Wallart from "./Wallart";
 import Wallpaper from "./Wallpaper";
-import Wardrobe from "./Wardrobe";
 import Sofa from "./Sofa";
 import Interior from "./Interior";
-import Laminatedwoodenflooring from "./Laminatedwoodenflooring";
+
 
 
 
@@ -31,11 +29,8 @@ const App =() => {
     <Route exact path="/contact" component={Contact}/>
     <Route exact path="/Login" component={Login}/>
     <Route exact path="/Signup" component={Signup}/>
-    <Route exact path="/Wallart" component={Wallart}/>
-    <Route exact path="/Laminatedwoodenflooring" component={Laminatedwoodenflooring}/>
     <Route exact path="/Wallpaper" component={Wallpaper}/>
     <Route exact path="/Sofa" component={Sofa}/>
-    <Route exact path="/Wardrobe" component={Wardrobe}/>
     <Route exact path="/Interior" component={Interior}/>
     <Redirect to="/"/>
 
